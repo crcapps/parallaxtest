446225aab2432fb247cd4aa591f8ab135abc49af
Casey Capps <crcapps@gmail.com>
2020-08-30 11:40:35 -0400

    device motion request

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
-const scene = document.getElementById('scene');
-const parallaxInstance = new Parallax(scene);
+DeviceMotionEvent.requestPermission()
+  .then(response => {
+    if (response == 'granted') {
+      const scene = document.getElementById('scene')
+      const parallaxInstance = new Parallax(scene)
+      return parallaxInstance
+    }
+  })
+  .catch(console.error)
