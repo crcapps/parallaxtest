2aa37a321704b789b3f1a75d62b04b039c69fdbe
Casey Capps <crcapps@gmail.com>
2020-08-30 11:44:05 -0400

    removed request

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-DeviceMotionEvent.requestPermission()
-  .then(response => {
-    if (response == 'granted') {
-      const scene = document.getElementById('scene')
-      const parallaxInstance = new Parallax(scene)
-      return parallaxInstance
-    } else {
-      console.warn("Motion denied!")
-    }
-  })
-  .catch(console.error)
+const scene = document.getElementById('scene')
+const parallaxInstance = new Parallax(scene)
+
